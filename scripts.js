45197f2adecf6fce03b4ad07da38ac04cfcf6625
Harendra Kumar <110033700+developerdatascience@users.noreply.github.com>
2025-01-23 22:50:42 +0530

    first commit
    added starter template

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const sidebar = document.getElementById('sidebar');
+const toggleIcon = document.getElementById('sidebar-toggle');
+
+toggleIcon.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+});
+
+
+// add another function
+
